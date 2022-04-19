989acc3b635310ae3214dc9d8819bb41ee1c3f32
amoro99 <andrew@entrinsik.com>
2022-04-19 12:01:26 -0400

    Better test, improve url building

--- test/module-spec.js
+++ test/module-spec.js
@@ -3,29 +3,59 @@
 const {expect} = require('chai');
 const SAML = require('../lib/saml').SAML;
 const url = require('url');
-
-
+const privateCert = require('./cert');
 describe('The passport-saml-encrypted-entrinsik module', async () => {
     it(`should merge an idp url's query string with it's SAMLRequest`, () => {
         const saml = new SAML({
             protocol: 'https://',
             path: 'someidp.com/saml/2.0/auth?id=foo',
             callbackUrl: 'https://generalsoftware.com/auth',
-            entryPoint: 'https://someidp.com/saml/2.0/auth?id=foo'
+            entryPoint: 'https://someidp.com/saml/2.0/auth?id=foo',
+            privateCert,
+            signatureAlgorithm: 'RSA-SHA256'
         });
 
 
         saml.getAuthorizeUrl(null, (err,the_url) => {
             const parsed = url.parse(the_url, true);
             expect(parsed.query.id).to.equal('foo');
-            expect(parsed.query.SAMLRequest).to.not.be.null;
+            expect(parsed.query.SAMLRequest).to.not.be.empty;
+            expect(parsed.query.SigAlg).to.not.be.empty;
+            expect(parsed.query.Signature).to.not.be.empty;
         })
 
         saml.getLogoutUrl({user:{nameIdFormat:'foo', nameID:'foo'}}, (err,the_url) => {
             const parsed = url.parse(the_url, true);
             expect(parsed.query.id).to.equal('foo');
-            expect(parsed.query.SAMLRequest).to.not.be.null;
+            expect(parsed.query.SAMLRequest).to.not.be.empty;
+            expect(parsed.query.SigAlg).to.not.be.empty;
+            expect(parsed.query.Signature).to.not.be.empty;
         })
 
     });
-})
+    it(`should behave normally if the idp url has no query string`, () => {
+        const saml = new SAML({
+            protocol: 'https://',
+            path: 'someidp.com/saml/2.0/auth',
+            callbackUrl: 'https://generalsoftware.com/auth',
+            entryPoint: 'https://someidp.com/saml/2.0/auth?id=foo',
+            privateCert,
+            signatureAlgorithm: 'RSA-SHA256'
+        });
+
+
+        saml.getAuthorizeUrl(null, (err,the_url) => {
+            const parsed = url.parse(the_url, true);
+            expect(parsed.query.SAMLRequest).to.not.be.empty;
+            expect(parsed.query.SigAlg).to.not.be.empty;
+            expect(parsed.query.Signature).to.not.be.empty;
+        })
+
+        saml.getLogoutUrl({user:{nameIdFormat:'foo', nameID:'foo'}}, (err,the_url) => {
+            const parsed = url.parse(the_url, true);
+            expect(parsed.query.SAMLRequest).to.not.be.empty;
+            expect(parsed.query.SigAlg).to.not.be.empty;
+            expect(parsed.query.Signature).to.not.be.empty;
+        })
+
+    });})
